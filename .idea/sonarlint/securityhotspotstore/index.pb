dc160fe18fd9677db272e75a6e326aa88900a079
Jelatinone <115746975+Jelatinone@users.noreply.github.com>
2023-03-20 20:49:11 -0500

    Changed from Integer to int Logic for Constructors

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+F
+src/main/java/maze.dat,d\5\d52441270e08b5f78848e1439a97130ab31b523d
+M
+src/main/java/MazeRunner.java,1\8\188730faee2e49a738f9a7e391bc5448b4d63622
+G
+src/main/java/Maze.java,3\6\36ac82fae9ab971ad936fd4c3be1600485ee27ac
--- src/main/java/Maze.java
+++ src/main/java/Maze.java
@@ -3,9 +3,10 @@
 //Name - Cody Washington
 
 //Libraries
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 
 /**
@@ -17,12 +18,12 @@ import java.util.stream.Collectors;
  */
 public class Maze extends Thread
 {
-    private Integer[][] maze;
-    public Boolean hasEntrance = false, hasExit = false, hasPath = false, isValid = false;
+    private final int[][] maze;
+    private ArrayList<Integer[][]> paths;
+    public Boolean hasEntrance = false, hasExit = false, hasPath = false, isValid;
     public Integer ShortestPath = -1;
-
     /** Default Constructor. */
-    public Maze() {maze = new Integer[0][0];}
+    public Maze() {maze = new int[0][0];}
 
     /** Constructor. 
      * @param size The size of the maze
@@ -30,50 +31,62 @@ public class Maze extends Thread
      */
     public Maze(int size, String line)
     {
-        maze = new Integer[size][size]; 
-        for(int i = 0, j = 0; i < size; i++)
+        if(line.length() >= (Math.pow(size,2) * 2) - 1)
         {
-            maze[i] = (Arrays.asList(line.substring(j,(j+size-1)).split(" "))).stream().map(s -> Integer.parseInt(s)).
-            collect(Collectors.toList()).stream().toArray(Integer[]::new);
-            if(Objects.equals(i,0)) {if(Objects.equals(maze[0][0],1) && getNumberValidDirections(0,0) != 0){hasEntrance = true;}}
-            if(Objects.equals(maze[i][maze.length-1],1) && getNumberValidDirections(i,maze.length-1) != 0) {hasExit = true;} 
-            if(Objects.equals(i,0)) {j+=4;} else {j+=5;}
+            maze = new int[size][size];
+            for(int i = 0, j = 0; i < size; i++, j += size * 2)
+            {maze[i] = Arrays.stream((line.substring(j , j + (size * 2)-1).trim().split("\\s+"))).mapToInt(Integer::parseInt).toArray();}
+            for(int i = 0; i < size; i++)
+            {if(maze[i][maze.length-1] == 1 && getNumberValidDirections(i,maze.length-1) != 0) {hasExit = true;}}
+            if(Objects.equals(maze[0][0],1) && getNumberValidDirections(0,0) != 0)
+            {hasEntrance = true;}
+            if(this.hasEntrance && this.hasExit)
+            {this.isValid = true; this.start();}
         }
-        if(this.hasEntrance && this.hasExit) {this.isValid = true; this.start();}
+        else {maze = new int[size][size];}
     }
 
     public void run()
     {
-        if(this.isAlive() && this.isValid)
-        {
-            Integer[] Results = checkForExitPath(0, 0);
-            ShortestPath = Results[1];
-            hasPath = convertToBoolean(Results[0]);
-        }
+        if(this.isAlive() && this.isValid) {int[] Results = checkForExitPath(0, 0); ShortestPath = Results[1]; hasPath = convertToBoolean(Results[0]);}
     }
 
     /**
      * Check is the maze(which contains an exit and entrance) contains a valid path
      * between the exit and entrance without using diagonals.
-     * @return If the recursive check has reached an exit in the last column
+     * @return An int array where the 0th element represents if a path has been found, and the 1st element represents the length of the shortest path
      */
-    public Integer[] checkForExitPath(int r, int c)
-    {
+    public int[] checkForExitPath(int r, int c) {
         //conditional to check that r and c are still within the maze and that the current value of the maze is still 1.
         //if conditional to see if the current value of c is at the end of the maze.  Change the value of exitFound.
         //else save the current position of the maze, mark the spot as visited, and make recursive calls to check each possible path
         //change the current value of the maze back to the saved value.
-
-        try {if(convertToBoolean(maze[r][c])) {maze[r][c] = 2;} else {return new Integer[] {0,Integer.MAX_VALUE};}}
-        catch(NullPointerException exception) {return new Integer[] {0,Integer.MAX_VALUE};}
-	    if(Objects.equals(c,maze[0].length)) {maze[r][c] = 3; return new Integer[] {1,1};}
+        try {
+            if (convertToBoolean(maze[r][c]))
+            {
+                maze[r][c] = 2;
+            }
+            else
+            {
+                return new int[]{0, Integer.MAX_VALUE};
+            }
+        }
+        catch(NullPointerException exception)
+        {
+            return new int[]{0,Integer.MAX_VALUE};
+        }
+        if(Objects.equals(c,maze.length))
+        {
+            maze[r][c] = 3;
+            return new int[] {1,1};
+        }
         Boolean[] validDirections = checkDirections(r,c);
-        if(validDirections[0]) {Integer[] result = checkForExitPath(r,c-1);result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
-        if(validDirections[1]) {Integer[] result = checkForExitPath(r+1,c); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
-        if(validDirections[2]) {Integer[] result = checkForExitPath(r,c+1); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
-        if(validDirections[3]) {Integer[] result = checkForExitPath(r-1,c); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
-        return new Integer[] {0,Integer.MAX_VALUE};
-}
+        if(validDirections[0]) {int[] result = checkForExitPath(r,c-  1); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
+        if(validDirections[1]) {int[] result = checkForExitPath(r + 1,c); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
+        if(validDirections[2]) {int[] result = checkForExitPath(r,c + 1); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
+        if(validDirections[3]) {int[] result = checkForExitPath(r - 1,c); result[1] = result[1]++; if(convertToBoolean(result[0]) && result[1] < ShortestPath) {return result;}}
+        return new int[] {0,Integer.MAX_VALUE};
+    }
 
     /**
      * Check which directions contain a valid Integer
@@ -83,22 +96,17 @@ public class Maze extends Thread
     public Boolean[] checkDirections(int r, int c)
     {
         Boolean[] ValidDirections = new Boolean[4];
-        try  {if(convertToBoolean(maze[r][c-1])) {ValidDirections[0] = true;} else {ValidDirections[0] = false;}} 
-        catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[0] = false;}
-        try {if(convertToBoolean(maze[r+1][c])) {ValidDirections[1] = true;} else {ValidDirections[1] = false;}} 
-        catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[1] = false;}
-        try {if(convertToBoolean(maze[r][c+1])) {ValidDirections[2] = true;} else {ValidDirections[2] = false;}} 
-        catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[2] = false;}
-        try {if(convertToBoolean(maze[r-1][c])) {ValidDirections[3] = true;} else {ValidDirections[3] = false;}} 
-        catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[3] = false;}
+        try {ValidDirections[0] = convertToBoolean(maze[r][c - 1]);} catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[0] = false;}
+        try {ValidDirections[1] = convertToBoolean(maze[r + 1][c]);} catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[1] = false;}
+        try {ValidDirections[2] = convertToBoolean(maze[r][c + 1]);} catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[2] = false;}
+        try {ValidDirections[3] = convertToBoolean(maze[r - 1][c]);} catch(ArrayIndexOutOfBoundsException exception) {ValidDirections[3] = false;}
         return ValidDirections;
     }
     /** @return The number of directions which contain a valid Integer */
     public Integer getNumberValidDirections(int r, int c)
     {
-        int Count = 0;
-        for(Boolean Result: checkDirections(r,c)) 
-        {if(Result) {Count++;}}return Count;
+        int Count = 0; for(Boolean Result: checkDirections(r,c))
+    {if(Result) {Count++;}} return Count;
     }
 
     /**  @return An Integer(1 or 0) converted to a Boolean */
@@ -107,9 +115,10 @@ public class Maze extends Thread
     public String toString()
     {
         StringBuilder output= new StringBuilder();
-        for (Integer[] integers : maze) {
-            for (Integer integer : integers)
-                output.append((Objects.equals(integer, 3)) ? ("\u001B[32mP") : (((Objects.equals(integer, 2) || (Objects.equals(integer, 1)) ? ("\u001b[31m1") : ("\u001b[31m0")))));
+        for (int[] row : maze)
+        {
+            for (int point : row)
+                output.append((Objects.equals(point, 3)) ? ("\u001B[32mP") : (((Objects.equals(point, 2) || (Objects.equals(point, 1)) ? ("\u001b[31m1") : ("\u001b[31m0")))));
             output.append("\n");
         }
         output.append((hasPath) ? ("exit found - " + ShortestPath + " STEPS") : ("exit not found"));
